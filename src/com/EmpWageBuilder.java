1d0bad98c04b7f46a350b412461c586385f2185c
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-09-24 01:19:26 +0530

    UC6

--- src/com/EmpWageBuilder.java
+++ src/com/EmpWageBuilder.java
@@ -2,16 +2,39 @@ package com;
 
 public class EmpWageBuilder {
 	  public static void main(String[] args) {
-			int IS_PERMANENT_EMPLOYEE = 1;
-			
-			double CHECK = Math.floor(Math.random()*10)%2;
-			if(CHECK== IS_PERMANENT_EMPLOYEE) {
-				System.out.println("permanent employee");
-			}
-			else {
-				System.out.println("NOT A PERMAMENT EMPLOYEE");
-			}
+		final  int IS_PERMANENT_EMPLOYEE = 1;
+		final  int IS_TEMPORARY_EMPLOYEE = 2;
 		
+			int EMP_RATE_PER_HOUR = 20;
+			int EMP_MAX_HOURS = 10;
+			int NO_OF_DAYS = 2;
+			int total_wage;
+			int emp_Hrs=0;
 			
-		}
+			int total_hrs = 0;
+			int total_days=0;
+			while(total_days<NO_OF_DAYS && total_hrs<EMP_MAX_HOURS) {
+				total_days++;
+			int CHECK = (int) Math.floor(Math.random()*10)%3;
+			switch(CHECK) {
+			case IS_PERMANENT_EMPLOYEE :
+				emp_Hrs = 8;
+			    break;
+			
+			    case IS_TEMPORARY_EMPLOYEE :
+				emp_Hrs =4;
+			    break;
+			
+			   default:
+				emp_Hrs = 0;
+			}
+			total_hrs+= emp_Hrs;
+			}
+			
+			total_wage=total_hrs*EMP_RATE_PER_HOUR;
+			
+			System.out.println(total_wage);
+			
+		
+	  }
 }
